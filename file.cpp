f35ef442038e4c111235d6355cc808767ad649a4
vanvo299 <luongvanvo29@gmail.com>
2024-03-08 14:47:30 +0700

    file test

---
+++ file.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+#define EPS 1e-6
+
+double tinh_Sin(double x) {
+    double q = 0;
+    double t = x;
+    int k = 1;
+
+    do {
+        q += t;
+        k += 2;
+        t = -t * pow(x, 2) / (k * (k - 1));
+    } while (fabs(t) > EPS);
+
+    return q;
+}
+
+int main() {
+    double x;
+
+    // Nhập giá trị góc ở dạng radian
+    printf("Nhap x (radian): ");
+    scanf("%lf", &x);
+
+    
+
+    // In giá trị của góc cả ở radian và độ
+  
+    // In giá trị của sin(x) tính bằng khai triển
+    printf("Gia tri cua sinx theo khai trien: %.6lf\n", tinh_Sin(x));
+
+    // In giá trị của sin(x) tính bằng hàm sin() trong thư viện math
+    printf("Gia tri sin cua may: %.6lf\n", sin(x));
+
+    return 0;
+}
